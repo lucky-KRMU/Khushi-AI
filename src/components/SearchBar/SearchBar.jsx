fdb03a219246bb0fc20abd91c2b7120c32c0eb1b
lucky-KRMU <2501730142@krmu.edu.in>
2026-01-04 11:46:27 +0000

    made basic folder structure

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../../CSS/SearchBar.css";
+
+
+function SearchBar () {
+    return(
+        <>
+            <div className="search-box">
+                <input type="text" name="" id="" />
+                <button id="go-btn">Go</button>
+            </div>
+        </>
+    );
+}
+
+export default SearchBar;
